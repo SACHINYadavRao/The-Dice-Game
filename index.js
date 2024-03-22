3417647a7a6bc4a314162fcb62162e0c2a38beeb
Sachin Yadav <SachinYadavRao@users.noreply.github.com>
2024-03-22 17:19:52 +0530

    Changing the image src attribute using querySelectorAll and setAttribute("src", randomImageSource2);

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1 - 6
 
-var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; // images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0]; // select the first <img> element in the HTML file.
+
+image1.setAttribute("src", randomImageSource); // set the source attribute of image1 to be equal to randomImageSource.
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; // 1 - 6
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; // images/dice1.png - images/dice6.png
+
+var image2 = document
+  .querySelectorAll("img")[1]
+  .setAttribute("src", randomImageSource2); // select the second <img> element in the HTML file. Set the source attribute of image2 to be equal to randomImageSource.
